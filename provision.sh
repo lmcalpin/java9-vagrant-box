2e322dcee60d63a8a5d0f526f1bacfab576fc8fb
Lawrence McAlpin <lmcalpin@gmail.com>
2015-11-20 23:09:18 -0500

    install eclipse

--- provision.sh
+++ provision.sh
@@ -1,4 +1,11 @@
 echo **************************
+echo Installing xfce4
+echo **************************
+sudo apt-get install gdm
+sudo dpkg-reconfigure gdm
+sudo apt-get install gnome-terminal
+
+echo **************************
 echo DOWNLOAD JAVA 9 INSTALLER
 echo **************************
 sudo add-apt-repository ppa:webupd8team/java
@@ -19,6 +26,14 @@ echo "JAVA_HOME=/usr/local/java/jdk1.9.0
 PATH=$PATH:$HOME/bin:$JAVA_HOME/bin
 export JAVA_HOME
 export PATH" >> /home/vagrant/.profile
+
+echo **************************
+echo INSTALL ECLIPSE
+echo **************************
+mkdir /usr/local/eclipse
+cd /usr/local/eclipse
+sudo wget http://mirror.cc.columbia.edu/pub/software/eclipse/eclipse/downloads/drops4/S-4.6M2-201509162000/eclipse-SDK-4.6M2-linux-gtk-x86_64.tar.gz
+tar xf eclipse-SDK-4.6M2-linux-gtk-x86_64.tar.gz
 
 echo **************************
 echo DONE
